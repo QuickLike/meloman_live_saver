ba1373f64531cf7a59e6a1f30b0b7bfba4251d52
quicklike <quicklikerfree@gmail.com>
2025-11-09 10:12:43 +0300

    not_finished

--- constants.py
+++ constants.py
@@ -6,22 +6,11 @@ load_dotenv('.env')
 
 
 HEADERS = {
-    'Accept': '*/*',
-    'Accept-Encoding': 'identity;q=1, *;q=0',
-    'Accept-Language': 'ru,en;q=0.9',
-    'Connection': 'keep-alive',
-    'Cookie': '_ym_uid=1730726793718272111; _ym_d=1730726793; _ym_isad=2',
-    'Host': 'records.meloman.facecast.net',
-    # 'If-Range': "6725ef19-f555ccff",
-    'Referer': 'https://player.meloman.facecast.net/',
-    'Sec-Fetch-Dest': 'video',
-    'Sec-Fetch-Mode': 'no-cors',
-    'Sec-Fetch-Site': 'same-site',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 '
-                  'YaBrowser/24.10.0.0 Safari/537.36',
-    'sec-ch-ua': '"Chromium";v="128", "Not;A=Brand";v="24", "YaBrowser";v="24.10", "Yowser";v="2.5"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': "Windows",
+    "referer":"https://player.meloman.facecast.net/",
+    "sec-ch-ua": '"Not)A;Brand";v="8", "Chromium";v="138", "YaBrowser";v="25.8", "Yowser";v="2.5"',
+    "sec-ch-ua-mobile": '?0',
+    "sec-ch-ua-platform": "Windows",
+    "user-agent":' Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 YaBrowser/25.8.0.0 Safari/537.36',
 }
 
 MAIN_URL = 'https://meloman.ru'
